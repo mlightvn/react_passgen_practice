29eb21ab6a82a81e1fcc8787afb77e966323f6c5
グエン・ナム <nguyen.nam@urban-funes.co.jp>
2019-10-24 15:03:14 +0900

    Social links

--- src/App.js
+++ src/App.js
@@ -86,7 +86,17 @@ function App() {
       </main>
       <footer>
         <div className="container">
-          Author: Nguyen Ngoc Nam
+          <div className="row">
+            Author:&nbsp;<a href="https://coxanh.coupon-pon.net/about" target="_blank" rel="noopener noreferrer" >Nguyen Ngoc Nam</a>
+          </div>
+          <div className="row">
+            Social:&nbsp;
+            <a href="https://www.linkedin.com/in/nguyenngocnam/" target="_blank" rel="noopener noreferrer" ><i className="fab fa-linkedin"></i> LinkedIn</a>
+            &nbsp;|&nbsp;
+            <a href="http://stackoverflow.com/users/6351894/ngoc-nam" rel="noopener noreferrer" ><span className="fab fa-stack-overflow"></span> StackOverFlow</a>
+            &nbsp;|&nbsp;
+            <a href="https://github.com/namtenten" target="_blank" rel="noopener noreferrer" ><span className="fab fa-github"></span> GitHub</a>
+          </div>
         </div>
       </footer>
     </>
